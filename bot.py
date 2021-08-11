0ec1390ba7be020cfb3ef989a3c5d42e25133bee
Paavo Gaidach <paavo.gaidach@gmail.com>
2021-08-11 16:43:37 +0300

    Added RSI chec+ placeholders for buy/sell logick

--- bot.py
+++ bot.py
@@ -1,7 +1,18 @@
-import websocket, json, pprint
+import websocket, json, pprint, talib, numpy
+
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+RSI_PERIOD = 4
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+
+TRADE_SYMBOL = 'ETHUSDT'
+#TEADE_QUANTITY 0.005
+
 
 closes = []
+
+in_position = False
+
 def on_open(ws):
     print('opened connection')
 
@@ -26,6 +37,26 @@ def on_message(ws, message):
         print("closes")
         print(closes)
         
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+            rsi = talib.RSI(np_closes, RSI_PERIOD)
+            print("all rsi:s calculated so far")
+            print(rsi)
+            last_rsi = rsi[-1]
+            print("the current rsi is {}".format(last_rsi))
+
+            if last_rsi > RSI_OVERBOUGHT:
+                if in_position:
+                    print ("Overbought, SELL SELL SELL!")
+                    #put binance sell logick here
+
+            if last_rsi < RSI_OVERSOLD:
+                if in_position:
+                    print("Oversold, but you already own it, nothing to do.")
+                else:
+                    print ("Oversold, BUY BUY BUY!")
+                    #put binance buy logick here
+
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 
 ws.run_forever()
