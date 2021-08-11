246488fe1ad3a2924e7d415f4a22737d6c571462
Paavo Gaidach <paavo.gaidach@gmail.com>
2021-08-11 15:32:49 +0300

    Added list of closing prices

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import websocket, json, pprint
-
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+closes = []
 def on_open(ws):
     print('opened connection')
 
@@ -9,10 +9,23 @@ def on_close(ws):
     print('Closed connection')
 
 def on_message(ws, message):
+    global closes
     print('recieved message')
     json_message = json.loads(message)
+    
     pprint.pprint(json_message)
 
+    candle = json_message['k']
+
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+    if is_candle_closed:
+        print("candle closed at {}".format(close))
+        closes.append(float(close))
+        print("closes")
+        print(closes)
+        
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 
 ws.run_forever()
