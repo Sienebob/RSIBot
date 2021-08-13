0f8e98fa67c1b77125495025c75e0e54866ccbbd
Paavo Gaidach <paavo.gaidach@gmail.com>
2021-08-13 23:52:32 +0300

    Instantiated client

--- bot.py
+++ bot.py
@@ -1,17 +1,23 @@
 import websocket, json, pprint, talib, numpy
+import config
+from binance.client import Client
+from binance.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+
 RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
-
 TRADE_SYMBOL = 'ETHUSDT'
 TEADE_QUANTITY = 0.005
 
-
 closes = []
-
 in_position = False
+
+client = Client(config.API_KEY, config.API_SECRET) #removed , tld='us'
+
+
+
 
 def on_open(ws):
     print('opened connection')
